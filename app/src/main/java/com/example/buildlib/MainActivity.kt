81db7b8aecba1dc413eced617d9a350892c2bef0
RichardDoan <richard@vinova.sg>
2023-10-10 10:05:42 +0700

    Initial commit

---
+++ app/src/main/java/com/example/buildlib/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.buildlib
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.buildlib.ui.theme.BuildLibTheme
+import com.example.demolib.ImagePreview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BuildLibTheme {
+                // A surface container using the 'background' color from the theme
+                ImagePreview(image = painterResource(id = R.mipmap.ic_demo),
+                description = "Demo Lib",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .size(150.dp))
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BuildLibTheme {
+        Greeting("Android")
+    }
+}
